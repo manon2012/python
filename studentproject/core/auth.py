518c34d218d1b89c3c2568d0045a9846053d9327
wupei <manon2012@126.com>
2019-11-20 22:40:56 +0800

    stproject

---
+++ studentproject/core/auth.py
@@ -0,0 +1,11 @@
+def login():
+    name = input("name:")
+    passwd = input("passwd:")
+    with open('userinfo') as f:
+        for line in f:
+            user,pwd,ident=line.strip().split("|")
+            if user==name and pwd==passwd:
+                print ("login ok")
+                print (user,ident)
+                return (user,ident)
+
