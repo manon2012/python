3274a5627a413865e59eece909ce91f0045443d7
e <manon2012@126.com>
2019-12-05 17:35:12 +0800

    1

--- work/Do/testhalfsort.py
+++ work/Do/testhalfsort.py
@@ -1,10 +1,23 @@
 
 a=[1,2,3,4,5,6,7,8,9,11,66,99,200,900]
 
-def halfsort(a):
-    mid =len(a)/2
-    left=0
-    right =len(a)
+def bs(a,x):
+    n=len(a)
+    first=0
+    last =n-1
 
-    if n> a[mid]:
-        left=mid
+    while first<=last:
+        mid = (first+last) // 2
+        if a[mid]==x:
+            return  True
+
+        elif x<a[mid]:
+            last=mid
+        else:
+            first=mid
+    return False
+
+
+if __name__ == '__main__':
+    #print (bs(a,9))
+    print (bs(a,100))
--- work/Do/testsuper.py
+++ work/Do/testsuper.py
@@ -1,3 +1,33 @@
+#
+# class Bird():
+#     def __init__(self):
+#         self.hungry=True
+#
+#     def eat(self):
+#         if self.hungry:
+#             print (" i eat")
+#             self.hungry=False
+#         else:
+#             print("thank you.")
+# class ABird(Bird):
+#     def __init__(self):
+#         #super().__init__()
+#         Bird.__init__(self)
+#         self.sound = "jjzz"
+#
+#     def song(self):
+#         print (self.sound)
+#
+#
+# b1=Bird()
+# b1.eat()
+# b1.eat()
+#
+# a1=ABird()
+# a1.song()
+# a1.eat()
+
+
 
 class Bird():
     def __init__(self):
@@ -5,24 +35,26 @@ class Bird():
 
     def eat(self):
         if self.hungry:
-            print (" i eat")
-            self.hungry=False
+            print (" i am eat")
+            self.hungry =False
         else:
-            print("thank you.")
-class ABird(Bird):
-    def __init__(self):
-        #super().__init__()
-        Bird.__init__(self)
-        self.sound = "jjzz"
+            print ('thank you')
 
+class StarBird(Bird):
+    def __init__(self):
+        #super(StarBird,self).__init__()
+        Bird.__init__(self)
+        self.sound ='lol'
     def song(self):
-        print (self.sound)
+        print ("i can %s"%self.sound)
 
 
 b1=Bird()
 b1.eat()
 b1.eat()
 
-a1=ABird()
-a1.song()
-a1.eat()
+b2=StarBird()
+b2.song()
+b2.song()
+
+b2.eat()
